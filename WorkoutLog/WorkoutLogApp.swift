8e8f9912fb2a6abea7b03bdb61639f5188be3712
ctn <35670719+TNg018@users.noreply.github.com>
2023-03-31 16:54:05 -0400

    Initial Commit

---
+++ WorkoutLog/WorkoutLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutLogApp.swift
+//  WorkoutLog
+//
+//  Created by Chloe on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
